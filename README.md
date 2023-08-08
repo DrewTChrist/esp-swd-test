29ae6ce9e05fb3431419b28716d95bb692c05399
Andrew Christiansen <andrewtaylorchristiansen@gmail.com>
2023-08-08 15:55:41 -0700

    Add comments

--- README.md
+++ README.md
@@ -1,1 +1,6 @@
 # esp32-swd-test
+
+This is a test of flashing a Raspberry Pi Pico with an Adafruit Huzzah32. The 
+esp32 does not natively support SWD. This is useful for boards like the Udoo 
+Key which have both an rp2040 and an esp32, where the esp32 is connected to 
+the SWD pins of the rp2040.
--- src/swd.rs
+++ src/swd.rs
@@ -9,6 +9,8 @@ use esp32_hal::{
     },
 };
 
+/// Esp32 gpio type with generic direction (DIR),
+/// signal (SIG) and pin number (NUM)
 type SwdPin<DIR, SIG, const NUM: u8> = GpioPin<
     DIR,
     Bank0GpioRegisterAccess,
@@ -18,24 +20,47 @@ type SwdPin<DIR, SIG, const NUM: u8> = GpioPin<
     NUM,
 >;
 
+/// Concrete clock pin type on pin 21
 type SwdClock = SwdPin<Output<esp32_hal::gpio::PushPull>, Gpio21Signals, 21>;
+/// Concrete data pin input type on pin 22
 type SwdDataIn = SwdPin<Input<esp32_hal::gpio::PullUp>, Gpio22Signals, 22>;
+/// Concrete data pin output type on pin 22
 type SwdDataOut = SwdPin<Output<esp32_hal::gpio::PushPull>, Gpio22Signals, 22>;
 
+/// Direction of the swd data pin
 enum DataPinMode {
+    /// Pull up input
     Input,
+    /// Push pull output
     Output,
 }
 
+/// Struct for holding esp32 peripherals 
+/// needed to bit bang the SWD protocol
+///
+/// There were two suitable methods for
+/// managing the direction change of the
+/// data pin. An enum with a variant for
+/// each direction that contains the pin
+/// type would have worked. In this case,
+/// I chose to use two fields each with
+/// their own input/output type and simply
+/// move the pin between those two fields.
 pub struct Swd {
+    /// Clock cycle pin
     clock_pin: Option<SwdClock>,
+    /// Data pin input
     data_in: Option<SwdDataIn>,
+    /// Data pin output
     data_out: Option<SwdDataOut>,
+    /// Delay peripheral for clock
     delay: Option<esp32_hal::Delay>,
+    /// Delay time
     delay_ms: u32,
 }
 
 impl Swd {
+    /// Creates a new swd struct
     pub fn new(clock_pin: SwdClock, data_pin: SwdDataOut, delay: esp32_hal::Delay) -> Self {
         Self {
             clock_pin: Some(clock_pin),
@@ -46,19 +71,25 @@ impl Swd {
         }
     }
 
+    /// Release the delay peripheral
     pub fn release_delay(&mut self) -> esp32_hal::Delay {
         core::mem::replace(&mut self.delay, None).unwrap()
     }
 
+    /// Release the clock pin
     pub fn release_clock_pin(&mut self) -> SwdClock {
         core::mem::replace(&mut self.clock_pin, None).unwrap()
     }
 
+    /// Release the data pin
     pub fn release_data_pin(&mut self) -> SwdDataOut {
         self.set_data_pin_mode(DataPinMode::Output);
         core::mem::replace(&mut self.data_out, None).unwrap()
     }
 
+    /// Reads the level of the data pin and
+    /// returns either an Option with the level
+    /// or None
     fn read_data_pin(&mut self) -> Option<u32> {
         if self.data_in.is_some() {
             let data_pin = self.data_in.as_mut().unwrap();
@@ -71,6 +102,7 @@ impl Swd {
         None
     }
 
+    /// Read bits on the data pin
     fn read_bits(&mut self, num_bits: usize) -> u32 {
         self.set_data_pin_mode(DataPinMode::Input);
         let mut result = 0;
@@ -84,6 +116,7 @@ impl Swd {
         result
     }
 
+    /// Sets the data pin either high or low
     fn write_data_pin(&mut self, bit: u32) {
         if self.data_out.is_some() {
             match bit {
@@ -94,6 +127,7 @@ impl Swd {
         }
     }
 
+    /// Write an idle state over the data line
     fn write_idle(&mut self) {
         self.write_bits(0, 8);
         _ = self.clock_pin.as_mut().unwrap().set_low();
@@ -101,6 +135,8 @@ impl Swd {
         _ = self.data_out.as_mut().unwrap().set_low();
     }
 
+    /// Write the clock low and high with a small
+    /// delay in between
     fn write_clock(&mut self) {
         if self.clock_pin.is_some() && self.delay.is_some() {
             _ = self.clock_pin.as_mut().unwrap().set_low();
@@ -110,6 +146,7 @@ impl Swd {
         }
     }
 
+    /// Writes some bits to the data pin
     fn write_bits(&mut self, mut bits: u32, num_bits: usize) {
         self.set_data_pin_mode(DataPinMode::Output);
         for _ in 0..num_bits {
@@ -119,6 +156,8 @@ impl Swd {
         }
     }
 
+    /// Changes the direction of the data pin to
+    /// either input or output
     fn set_data_pin_mode(&mut self, pin_mode: DataPinMode) {
         match pin_mode {
             DataPinMode::Input => if self.data_out.is_some() {
